6d668056769180a557f7e84fae035b4a81cee13a
Nikodem Eluszkiewicz <nikodem.eluszkiewicz.stud@pw.edu.pl>
2022-06-18 16:03:29 +0200

    mediana, min, max

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 import csv
-
+import statistics
 import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
     with open("accelerometer.csv") as accelerometer_file:
         accelerometer = csv.DictReader(accelerometer_file, delimiter=',')
-        s = 0.0
         rows = []
         n = 0
         ns = []
         for row in accelerometer:
-            s += float(row["Acceleration z (m/s^2)"])
             ns.append(n)
             rows.append(float(row["Acceleration z (m/s^2)"]))
             print(f"n: {n} {rows[n]} m/s^2")
             n += 1
         plt.plot(ns, rows)
         # print(f"Średnie przyspieszenie to {s/n} m/s\N{SUPERSCRIPT TWO}")
-        print(f"Średnie przyspieszenie to {s / n} m/s^2")
+        print(f"Średnia wartość przyspieszenia to {statistics.fmean(rows)} m/s^2")
+        print(f"Maksymalna wartość przyepieszenia to {max(rows)} m/s^2")
+        print(f"Minimalna wartość przyspieszenia to {min(rows)} m/s^2")
+        print(f"Mediana wartości przyspieszenia to {statistics.median(rows)} m/s^2")
         plt.show(block=False)
         plt.savefig("plot.png")
